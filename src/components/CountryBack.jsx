14b1b61fe1c8ec64bcb7b5c0a817ee4784cdbe1b
Rafal <rafalstefanowicz.rscode@gmail.com>
2023-11-24 11:16:42 +0100

    add styles to countryBack button

--- src/components/CountryBack.jsx
+++ src/components/CountryBack.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useParams, Link } from "react-router-dom";
-
+import { useParams, useNavigate } from "react-router-dom";
+import { FaPlaneDeparture } from "react-icons/fa";
 import "../../styles/countryBack.css";
 
 const CountryBack = () => {
   const { country } = useParams();
   const [isFixed, setIsFixed] = useState(false);
   const markerRef = useRef(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const ref = markerRef?.current;
@@ -26,11 +27,18 @@ const CountryBack = () => {
     };
   }, []);
 
+  //to do - adjust styles with map button
   return (
     <div className="countryBack" ref={markerRef}>
-      <Link to={`/?search=${country}`}>
-        <button className={isFixed ? "backButtonFixed" : ""}>Map</button>
-      </Link>
+      <button
+        onClick={() => {
+          navigate(`/?search=${country}`);
+        }}
+        className={`backButton ${isFixed ? "backButtonFixed" : ""}`}
+      >
+        <FaPlaneDeparture className="buttonIcon" />
+        Map
+      </button>
     </div>
   );
 };
