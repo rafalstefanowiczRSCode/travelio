fcfab1293a10f876d0dcb9ac198aac85c08b5bb8
Rafal <rafalstefanowicz1991@gmail.com>
2023-12-04 07:29:48 +0100

    add break line

---
+++ src/components/ToggleUnsplash.jsx
@@ -0,0 +1,11 @@
+import "../styles/toggleUnsplash.css";
+
+const ToggleUnsplash = () => {
+  return (
+    <>
+      <div className="breakLine"></div>
+      {/* <p>powered by unsplash</p> */}
+    </>
+  );
+};
+export default ToggleUnsplash;
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -4,13 +4,17 @@ import CountryImages from "../components/CountryImages";
 import CountryBack from "../components/CountryBack";
 import UnsplashImages from "../components/UnsplashImages";
 import MyImages from "../components/MyImages";
+import ToggleUnsplash from "../components/ToggleUnsplash";
+import { useState } from "react";
 
 const Country = () => {
+  const [showUnsplash, setShowUnsplash] = useState(false);
   return (
     <div className="countryPage">
       <CountryBack />
       <CountryInfo />
-      {/* <MyImages /> */}
+      <MyImages />
+      <ToggleUnsplash />
       <UnsplashImages />
     </div>
   );
