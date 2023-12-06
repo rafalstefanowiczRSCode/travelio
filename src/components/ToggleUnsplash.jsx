abea159faee6945a23f6f98eaf0a109ca48b6b35
Rafal <rafalstefanowicz.rscode@gmail.com>
2023-12-06 11:24:27 +0100

    add toggle unsplash functionality

--- src/components/ToggleUnsplash.jsx
+++ src/components/ToggleUnsplash.jsx
@@ -1,11 +1,23 @@
 import "../styles/toggleUnsplash.css";
+import { FaUnsplash } from "react-icons/fa6";
 
-const ToggleUnsplash = () => {
+const ToggleUnsplash = ({ onToggleClick, unsplashVisible }) => {
   return (
-    <>
-      <div className="breakLine"></div>
-      {/* <p>powered by unsplash</p> */}
-    </>
+    <div className="toggleUnsplash">
+      {!unsplashVisible ? (
+        <button className="toggleButton" onClick={onToggleClick}>
+          <span>Show Unsplash Images</span>
+        </button>
+      ) : (
+        <>
+          <div className="breakLine"></div>
+          <h2 className="unsplash">
+            <FaUnsplash className="unsplashIcon" />
+            Unsplash
+          </h2>
+        </>
+      )}
+    </div>
   );
 };
 export default ToggleUnsplash;
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,22 +1,41 @@
 import transition from "../transition";
 import CountryInfo from "../components/CountryInfo";
-import CountryImages from "../components/CountryImages";
 import CountryBack from "../components/CountryBack";
 import UnsplashImages from "../components/UnsplashImages";
 import MyImages from "../components/MyImages";
 import ToggleUnsplash from "../components/ToggleUnsplash";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "../styles/country.css";
+import { useParams } from "react-router-dom";
+import { rSCodeCountryList } from "../utils/countryList";
 
 const Country = () => {
-  const [showUnsplash, setShowUnsplash] = useState(false);
+  const { country } = useParams();
+  const isVisited = useMemo(
+    () =>
+      !!rSCodeCountryList.find((visitedCountry) => country === visitedCountry),
+    [country]
+  );
+  const [unsplashVisible, setUnsplashVisible] = useState(!isVisited);
+  const toggleUnsplashVisible = () => {
+    setUnsplashVisible((prev) => !prev);
+  };
+
   return (
     <div className="countryPage">
       <CountryBack />
       <CountryInfo />
-      <MyImages />
-      <ToggleUnsplash />
-      <UnsplashImages />
+      {isVisited && (
+        <>
+          <MyImages />
+          <ToggleUnsplash
+            unsplashVisible={unsplashVisible}
+            onToggleClick={toggleUnsplashVisible}
+          />
+        </>
+      )}
+
+      {unsplashVisible && <UnsplashImages />}
     </div>
   );
 };
